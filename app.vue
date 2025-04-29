184787b352b0cc984e5453be902019a6e3ef78be
Dragan Apostolski <drapostolski@gmail.com>
2025-04-29 16:22:08 +0200

    Setup project

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-7xl text-center">Dragan Apostolski</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+  
+  css: ['~/assets/css/main.css'],
+  
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    },
+  },
+  
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
